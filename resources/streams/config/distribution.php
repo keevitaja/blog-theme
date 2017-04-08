b7551f9767fa8391f238fcc66d6ac33710cbbd24
Tanel Tammik <keevitaja@gmail.com>
2017-04-08 16:53:00 +0300

    We only care about looks!

--- resources/streams/config/distribution.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'name'        => 'PyroCMS',
-    'description' => 'Build better Laravel websites and applications faster with Pyro.',
-    'version'     => 'v3.3.3',
-];
--- webpack.mix.js
+++
@@ -1,5 +0,0 @@
-const { mix } = require('laravel-mix');
-
-mix.js('resources/assets/js/app.js', 'compiled')
-    .sass('resources/assets/sass/app.scss', 'compiled')
-    .version();
