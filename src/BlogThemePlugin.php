522b32d6772ec1be61f67214d5c874ed845a399d
Tanel Tammik <keevitaja@gmail.com>
2018-08-07 21:11:45 +0300

    remove crisp && fix code

--- src/BlogThemePlugin.php
+++ src/BlogThemePlugin.php
@@ -20,14 +20,6 @@ class BlogThemePlugin extends Plugin
             new Twig_SimpleFunction('images', function($type, $key = 'post') {
                 return app(GetImages::class, ['type' => $type, 'key' => $key]);
             }),
-            new Twig_SimpleFunction('code', function($data) {
-                $replace = '<pre><code class="language-$1">$2</code></pre>';
-                $filtered = preg_filter("/::code-([a-z]+)\n((.|\n)*?)\n::/", $replace, (string)$data);
-
-                if (strlen($filtered) == 0) return $data;
-
-                return $filtered;
-            }),
             new Twig_SimpleFunction('dd', function($variable) {
                 return dd($variable);
             }),
